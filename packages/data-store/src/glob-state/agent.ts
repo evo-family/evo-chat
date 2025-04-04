b7a3316f386329e2836235a0dfa21b6b36d38bef
hibakanji <15443578+hibakanji@user.noreply.gitee.com>
2025-04-04 22:32:17 +0800

    chore: 优化agents数据初始化和diff的逻辑

--- packages/data-store/src/glob-state/agent.ts
+++ packages/data-store/src/glob-state/agent.ts
@@ -1,39 +1,30 @@
-import { IAssistant } from '@evo/types';
 import { persistenceTissue, persistenceTissueSync } from '@evo/utils';
+
+import { IAssistant } from '@evo/types';
 
 const AGENT_CACHE_KEY = `__agent_cache_key__`;
 
 export const agentsData = persistenceTissueSync<Record<string, IAssistant>>(AGENT_CACHE_KEY);
 
-export const gentsDataInit = persistenceTissue<Record<string, IAssistant>>(AGENT_CACHE_KEY);
+export const agentsDataInit = persistenceTissue<Record<string, IAssistant>>(AGENT_CACHE_KEY);
 
-const deferExecution = (callback: () => void) => {
-  if (typeof requestIdleCallback === 'function') {
-    requestIdleCallback(callback);
-  } else if (typeof requestAnimationFrame === 'function') {
-    requestAnimationFrame(callback);
-  } else {
-    setTimeout(callback, 0);
-  }
-};
-
-deferExecution(async () => {
+const initOrDiffAgentData = async () => {
   try {
     // 初始化并与本地缓存数据进行比较和修正
     const [cacheTissue, result] = await Promise.all([
-      gentsDataInit,
+      agentsDataInit,
       import('../constants/agent/agents.json'),
     ]);
 
     if (!result?.default?.length) return;
 
-    const unicodeMap = new Map<string, any>();
+    const uniqueMap = new Map<string, any>();
 
     (result.default as unknown as IAssistant[]).forEach((item) => {
-      if (unicodeMap.has(item.identifier)) {
+      if (uniqueMap.has(item.identifier)) {
         console.warn('agents.json 中 identifier 不能重复', item);
       } else {
-        unicodeMap.set(item.identifier, item);
+        uniqueMap.set(item.identifier, item);
       }
 
       const existValue = cacheTissue.getCellValueSync(item.identifier);
@@ -50,8 +41,17 @@ deferExecution(async () => {
       }
     });
 
-    unicodeMap.clear();
+    uniqueMap.clear();
   } catch (error) {
     console.error(error);
+  }
+};
+
+agentsDataInit.then((cacheTissue) => {
+  // 如果缓存数据为空，立即进行初始化，如果不为空，延迟2秒后进行diff操作.（减少首屏初始化的压力）
+  if (cacheTissue.getCellsValue({ all: true }).array.length) {
+    setTimeout(initOrDiffAgentData, 2e3);
+  } else {
+    initOrDiffAgentData();
   }
 });
