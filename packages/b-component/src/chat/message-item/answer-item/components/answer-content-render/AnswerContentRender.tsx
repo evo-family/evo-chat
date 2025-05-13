69a15c57a25ca5ed61d71daad692a319183bebf7
hibakanji <15443578+hibakanji@user.noreply.gitee.com>
2025-05-14 00:05:58 +0800

    feat: 修复有时候刷新页面不展示消息内容的bug

--- packages/b-component/src/chat/message-item/answer-item/components/answer-content-render/AnswerContentRender.tsx
+++ packages/b-component/src/chat/message-item/answer-item/components/answer-content-render/AnswerContentRender.tsx
@@ -1,24 +1,49 @@
+import { EModalConnStatus, useChatAnswerCtx, useChatAnswerOrgCtx } from '@evo/data-store';
+
 import { BubbleChat } from '@/chat/bubble-chat/BubbleChat';
 import React from 'react';
+import { useCellValueSelector } from '@evo/utils';
 
 export interface IReasoningRenderProps {
-  answerContent?: string;
+  turnIndex: number;
 }
 
 export const AnswerContentRender = React.memo<IReasoningRenderProps>((props) => {
-  const { answerContent } = props;
+  const { turnIndex } = props;
 
-  if (!answerContent) return null;
+  const chatTurnsCell = useChatAnswerOrgCtx((ctx) => ctx.chatTurnsCell);
 
-  return (
-    <BubbleChat
-      contents={[
-        {
-          key: 3,
-          role: 'user',
-          content: answerContent,
-        },
-      ]}
-    />
+  const [content] = useCellValueSelector(chatTurnsCell, (value) => value.at(turnIndex)?.content);
+  const [status] = useCellValueSelector(chatTurnsCell, (value) => value.at(turnIndex)?.status);
+  const [errorMessage] = useCellValueSelector(
+    chatTurnsCell,
+    (value) => value.at(turnIndex)?.errorMessage
   );
+
+  if (status === EModalConnStatus.ERROR) {
+    return (
+      <BubbleChat
+        contents={[
+          {
+            key: 3,
+            role: 'error',
+            content: errorMessage,
+          },
+        ]}
+      />
+    );
+  } else {
+    return (
+      <BubbleChat
+        contents={[
+          {
+            key: 3,
+            role: 'user',
+            content: content,
+            loading: status === EModalConnStatus.PENDING,
+          },
+        ]}
+      />
+    );
+  }
 });
--- packages/data-store/src/react-context/answer.tsx
+++ packages/data-store/src/react-context/answer.tsx
@@ -1,10 +1,16 @@
-import { IModelAnserActionRecord, TModelAnswer, TModelAnswerCell } from '@/chat-message/types';
-import React, { PropsWithChildren, useLayoutEffect, useMemo, useState } from 'react';
+import {
+  EChatAnswerStatus,
+  IModelAnserActionRecord,
+  TModelAnswer,
+  TModelAnswerCell,
+} from '@/chat-message/types';
+import React, { PropsWithChildren, useEffect, useLayoutEffect, useMemo, useState } from 'react';
 import { useGetState, useUnmount } from 'ahooks';
 
 import { DataCell } from '@evo/utils';
 import { createContext } from 'use-context-selector';
 import { createUseContextSelector } from '@/utils/createContextSelector';
+import { debounce } from 'lodash';
 import { useChatMsgCtx } from './message';
 
 export interface IChatAnswerContextProps {
@@ -14,8 +20,7 @@ export interface IChatAnswerContextProps {
 export interface IChatAnswerContext {
   answerCell: TModelAnswerCell;
   chatTurnsCell: DataCell<IModelAnserActionRecord['chatTurns']>;
-  curHisIndex: number | undefined;
-  setCurHisIndex: (index: number) => void;
+  curHisIndex: DataCell<number | undefined>;
 }
 
 const emptyMsgConfig: TModelAnswer = {
@@ -24,13 +29,13 @@ const emptyMsgConfig: TModelAnswer = {
   provider: 'test',
   createdTime: +Date.now(),
   histroy: [],
+  status: EChatAnswerStatus.END,
 };
 const emptyChatTurns: IModelAnserActionRecord['chatTurns'] = [];
 const defaultContext: IChatAnswerContext = {
   answerCell: new DataCell(emptyMsgConfig),
   chatTurnsCell: new DataCell(emptyChatTurns),
-  curHisIndex: undefined,
-  setCurHisIndex: () => {},
+  curHisIndex: new DataCell<undefined | number>(undefined),
 };
 
 export const ChatAnswerContext = createContext(defaultContext);
@@ -49,13 +54,13 @@ export const ChatAnswerContextProvider = React.memo<PropsWithChildren<IChatAnswe
       chatMsg?.modelAnswers.getCellSync(answerId)
     );
     const [chatTurnsCell] = useState(() => new DataCell<IModelAnserActionRecord['chatTurns']>([]));
-    const [curHisIndex, setCurHisIndex, getCurHisIndex] = useGetState(() =>
-      answerCell ? answerCell.get().histroy.length - 1 : undefined
+    const [curHisIndex] = useGetState(
+      () => new DataCell(answerCell ? answerCell.get().histroy.length - 1 : undefined)
     );
 
     const contextValue: IChatAnswerContext = useMemo(() => {
-      return { answerCell: answerCell as any, chatTurnsCell, curHisIndex, setCurHisIndex };
-    }, [answerCell, chatTurnsCell, curHisIndex, setCurHisIndex]);
+      return { answerCell: answerCell as any, chatTurnsCell, curHisIndex };
+    }, [answerCell, chatTurnsCell, curHisIndex]);
 
     useLayoutEffect(() => {
       chatMsg.modelAnswers.getCellUntil({ key: answerId }).then(setAnswerCell);
@@ -64,21 +69,24 @@ export const ChatAnswerContextProvider = React.memo<PropsWithChildren<IChatAnswe
     useLayoutEffect(() => {
       if (!answerCell) return;
 
-      const subscription = answerCell.listen(
-        (notice) => {
-          if (notice.action === 'destroy') return;
+      // 默认展示最后一条记录
+      curHisIndex.set(answerCell.get().histroy.length - 1);
 
-          const curIndex = getCurHisIndex();
-          if (curIndex === undefined) return;
+      const computChatTurns = () => {
+        const curIndex = curHisIndex.get();
+        if (curIndex === undefined) return;
 
-          const { next } = notice;
-          const nextChatTurns = next.histroy.at(curIndex)?.chatTurns ?? [];
-          chatTurnsCell.set([...nextChatTurns]);
-        },
-        { immediate: true }
-      );
+        const curHis = answerCell.get().histroy.at(curIndex);
+        chatTurnsCell.set(curHis ? curHis.chatTurns.slice() : []);
+      };
 
-      return subscription?.unsubscribe;
+      const subscription1 = curHisIndex.listen(computChatTurns);
+      const subscription2 = answerCell.listen(computChatTurns, { immediate: true });
+
+      return () => {
+        subscription1.unsubscribe();
+        subscription2.unsubscribe();
+      };
     }, [curHisIndex, answerCell]);
 
     // 所有组件卸载时的销毁逻辑写这里
